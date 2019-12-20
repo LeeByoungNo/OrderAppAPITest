c896604157dbf7162b1b67cb9f4b920110577549
LeeByoungNo <leebn0819@hanmail.net>
2019-12-20 10:07:51 +0900

    test

--- app/src/main/java/com/example/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/myapplication2/MainActivity.java
@@ -22,5 +22,32 @@ public class MainActivity extends AppCompatActivity {
 //        Build.VERSION_CODES.LOLLIPOP_MR1 ;
 //        Build.VERSION.SDK_INT ;
         // WebView remote debug 설정
+
+        /*
+        *  var post_data = "uid="+req.body.uid ;
+           var post_options = setPostOption("/api/storeDetail.json",post_data,req.body.loginKey);
+           *
+           var post_data = "type="+req.body.type+"&dong="+req.body.dong+"&x="+req.body.x+"&y="+req.body.y+"&spName="+req.body.spName
+                 +"&pageNo="+req.body.pageNo+"&pageSize="+req.body.pageSize ;
+
+           var post_options = setPostOption("/api/storeListDeli.json",post_data,req.body.loginKey);
+           *
+           var post_data = "&x="+req.body.x+"&y="+req.body.y+"&pageSize="+req.body.pageSize ;
+           var post_options = setPostOption("/api/storeListDeliAll.json",post_data,"");
+           *
+           * lkj1212@daum.net / rkddb1212
+           *  x : 37.4732219 , y : 126.8843009
+           * https://m.delivera.co.kr/api/ + API 명
+           *
+           * login.json : userId, userPwd
+           *
+           * reviewInsert.json : loginKey, userId,content,spCode,grade,file
+           * noticeList.json : loginKey
+           * noticeDetail.json : loginKey , nid
+           * eventList.json : loginKey , spCode
+           * categoryList.json : GET
+           * storeDetail.json : uid (상점ID)
+           * showReviewList.json : spCode, pageSize, pageNo
+        * */
     }
 }
